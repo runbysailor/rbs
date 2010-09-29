faae95706e726e0956fec2edfbfce90d1e8c37ee
Run by Sailor <admin@runbysailor.com>
2010-09-29 00:42:59 -0500

    Initial commit of rbs index page.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '5394105a70b7414ff9dd70b8e0422246d64d72447788261c215bf40597f75a61f568fbb2a8636cf424b075cf4fa2aea63868d92b3bf6d71ba29f8a9d0a163445'
